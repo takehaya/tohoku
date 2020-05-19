a12e3a922cd0ae243eaea6b91450d227866c03bd
Takeru Hayasaka <hayatake396@gmail.com>
2020-05-20 03:24:08 +0900

    update file

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/tohoku/MainService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/tohoku/MainService.java
@@ -13,6 +13,9 @@ import com.google.zxing.common.HybridBinarizer;
 import org.opencv.aruco.Aruco;
 import org.opencv.aruco.Dictionary;
 import org.opencv.core.Mat;
+import org.opencv.calib3d.Calib3d;
+//import org.opencv.android.Utils;
+//import org.opencv.objdetect.QRCodeDetector;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -86,73 +89,86 @@ public class MainService extends KiboRpcService {
         String p2_1_con[] = new String[2];
         String p2_2_con[] = new String[2];
         String p2_3_con[] = new String[2];
+
+        double px3 = 0,py3 = 0,pz3=0,qx3 = 0,qy3=0,qz3=0;
         int arv = 0;
 
 
-//        moveTo(10.6, -4.3, 5, 0, 0, -0.7071068, 0.7071068);
+        Vec3 road1_1=new Vec3(11.15,-4.8,4.55);
+        Vec3 target1_3=new Vec3(11,-5.5,4.55-0.1);
+        Vec3 target1_1=new Vec3(11.3+0.1,-5.7,4.5);
+        Vec3 target1_2=new Vec3(11,-6,5.35+0.1);
 
-        moveTo(10.6, -4.3, 5, 0, 0, -0.7071068, 0.7071068);
-        moveTo(10.6, -5.7, 5, 0, 0, -0.7071068, 0.7071068);
-
-        Log.i(LOGTAG,"start scan 1");
-        String p1_1 = scanBarcode(11.5 , -5.7-0.5, 4.5, 0, 0, 0, 1,0);
-        p1_1_con = p1_1.split(", ");
-        double px3 = Double.parseDouble(p1_1_con[1]);
-        Log.d(LOGTAG,"p1_1 = " + p1_1);
-        moveTo(11, -6, 5.55, 0, -0.7071068, 0, 0.7071068);
-
-
-        Log.d(LOGTAG,"start scan 2");
-        String p1_3 = scanBarcode(11, -6, 5.55, 0, -0.7071068, 0, 0.7071068,1);
-        p1_3_con = p1_3.split(", ");
-        double pz3 = Double.parseDouble(p1_3_con[1]);
+        moveTo(road1_1, new WrapQuaternion(0, 0, 0.707f, -0.707f));
+        moveTo(target1_3, new WrapQuaternion(0, 0.707f, 0, 0.707f));
+        String p1_3 = scanBarcode(2);
         Log.d(LOGTAG,"p1_3 = " + p1_3);
+        if (p1_3 != "error"){
+            p1_3_con = p1_3.split(", ");
+            pz3 = Double.parseDouble(p1_3_con[1]);
+        }
 
+        moveTo(target1_1, new WrapQuaternion(0, 0, 0, -1));
+        String p1_1 = scanBarcode(0);
+        Log.d(LOGTAG,"p1_1 = " + p1_1);
+        if (p1_1 != "error"){
+            p1_1_con = p1_1.split(", ");
+            px3 = Double.parseDouble(p1_1_con[1]);
+        }
 
-//        Log.d(LOGTAG,"start scan 3");
-//        String p1_2 = scanBarcode(11, -6, 5.55, 0, -0.7071068, 0, 0.7071068,1);
-//        p1_2_con = p1_2.split(", ");
-//        double py3 = Double.parseDouble(p1_2_con[1]);
-//        Log.d(LOGTAG,"p1_2 = " + p1_2);
-//
-//
-//        moveTo(10.50,-6.45,5.40,0,0,0,0);
-//
-//        Log.d(LOGTAG,"start scan 4");
-//        String p2_2 = scanBarcode(11.49,-8,5,0,0,0,1,4);
-//        p2_2_con = p2_2.split(", ");
-//        double qz3 = Double.parseDouble(p2_2_con[1]);
-//        Log.d(LOGTAG,"p1_2 = " + p2_2);
-//
-//
-//        Log.d(LOGTAG,"start scan 5");
-//        String p2_3 = scanBarcode(11,-7.7,5.44,0.7,0,0.7,0 ,5);
-//        p2_3_con = p2_3.split(", ");
-//        double qy3 = Double.parseDouble(p2_3_con[1]);
-//        Log.d(LOGTAG,"p2_2 = " + p2_3);
-//
-//
-//        Log.d(LOGTAG,"start scan 6");
-//        String p2_1 = scanBarcode(10.41,-7.5,4.7,0,0,1,0,3);
-//        p2_1_con = p2_1.split(", ");
-//        double qx3 = Double.parseDouble(p2_1_con[1]);
-//        Log.d(LOGTAG,"p2_1 = " + p2_1);
-//
-//
-//        moveTo(px3,py3,pz3,qx3,qy3,qz3,Math.sqrt(1 - (qx3 * qx3) - (qy3 * qy3) - (qz3 * qz3)));
-//        Mat ids = new Mat();
-//        while (arv == 0) {
-//            Mat source = api.getMatNavCam();
-//            Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-//            List<Mat> corners = new ArrayList<>();
-//            Aruco.detectMarkers(source, dictionary, corners, ids);
-//            arv = (int) ids.get(0, 0)[0];
-//        }
-//
-//        api.judgeSendDiscoveredAR(Integer.toString(arv));
+        moveTo(target1_2, new WrapQuaternion(0, -0.707f, 0, 0.707f));
+        String p1_2 = scanBarcode(1);
+        Log.d(LOGTAG,"p1_2 = " + p1_2);
+        if (p1_2 != "error"){
+            p1_2_con = p1_2.split(", ");
+            py3 = Double.parseDouble(p1_2_con[1]);
+        }
+
+        Vec3 road2_1=new Vec3(10.5,-6.45,5.1);
+        Vec3 road2_2=new Vec3(11.35,-7.2,4.9);
+        Vec3 target2_1=new Vec3(10.48,-7.5,4.7);
+        Vec3 target2_3=new Vec3(11,-7.7,5.37);
+        Vec3 target2_2=new Vec3(11.32,-8,5);
+
+        moveTo(road2_1,new WrapQuaternion(0, 0, 0.707f, -0.707f));
+        moveTo(road2_2,new WrapQuaternion(0, 0, 0.707f, -0.707f));
+        moveTo(target2_1,new WrapQuaternion(0, 0, 0,1));
+        String p2_1 = scanBarcode(3);
+        Log.d(LOGTAG,"p2_1 = " + p2_1);
+        if (p2_1 != "error"){
+            p2_1_con = p2_1.split(", ");
+            qx3 = Double.parseDouble(p2_1_con[1]);
+        }
+
+        moveTo(target2_2,new WrapQuaternion(0, 0, 0,-1));
+        String p2_2 = scanBarcode(4);
+        Log.d(LOGTAG,"p2_2 = " + p2_2);
+        if (p2_2 != "error"){
+            p2_2_con = p2_2.split(", ");
+            qy3 = Double.parseDouble(p2_2_con[1]);
+        }
+
+        moveTo(target2_3,new WrapQuaternion(0, -0.707f, 0, 0.707f));
+        String p2_3 = scanBarcode(5);
+        Log.d(LOGTAG,"p2_3 = " + p2_3);
+        if (p2_3 != "error"){
+            p2_3_con = p2_3.split(", ");
+            qz3 = Double.parseDouble(p2_3_con[1]);
+        }
+
+        moveTo(px3,py3,pz3,qx3,qy3,qz3,Math.sqrt(1 - (qx3 * qx3) - (qy3 * qy3) - (qz3 * qz3)));
+        Mat ids = new Mat();
+        while (arv == 0) {
+            Mat source = api.getMatNavCam();
+            Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+            List<Mat> corners = new ArrayList<>();
+            Aruco.detectMarkers(source, dictionary, corners, ids);
+            arv = (int) ids.get(0, 0)[0];
+        }
+
+        api.judgeSendDiscoveredAR(Integer.toString(arv));
         api.laserControl(true);
         api.judgeSendFinishSimulation();
-
 
     }
 
@@ -164,16 +180,31 @@ public class MainService extends KiboRpcService {
 
     //-------------QR Decoding--------------------
 
+    private String scanBarcode(int no){
+        Log.d(LOGTAG,"start scanBarcode");
+        Bitmap snapshot = api.getBitmapNavCam();
+        String value = detectQrcode(snapshot);
+        if (value != "error") {
+            api.judgeSendDiscoveredQR(no , value);
+            Log.d(LOGTAG,"valuesQR" + value);
+        }
+        else{
+            Log.d(LOGTAG,"valuesQR = error");
+        }
+        return value;
+    }
+
     private String scanBarcode(double pos_x, double pos_y, double pos_z,
                                double qua_x, double qua_y, double qua_z,
                                double qua_w,int no){
+        Log.d(LOGTAG,"start scanBarcode");
         int loopMax = 10;
         int loop = 0;
         moveTo(pos_x,pos_y,pos_z,qua_x,qua_y,qua_z,qua_w);
         Bitmap snapshot = api.getBitmapNavCam();
         String value = detectQrcode(snapshot);
         double viewP = 0.025;
-        while (value == null && loop < loopMax) {
+        while (value == "error" && loop < loopMax) {
             if (loop%8 == 0) moveTo(pos_x,pos_y,pos_z,qua_x + viewP,qua_y,qua_z,qua_w);
             else if (loop%8 == 1) moveTo(pos_x,pos_y,pos_z,qua_x,qua_y + viewP,qua_z,qua_w);
             else if (loop%8 == 2) moveTo(pos_x,pos_y,pos_z,qua_x,qua_y,qua_z + viewP,qua_w);
@@ -187,15 +218,17 @@ public class MainService extends KiboRpcService {
             value = detectQrcode(snapshot);
             loop++;
         }
-        if (value != null) {
+        if (value != "error") {
             api.judgeSendDiscoveredQR(no , value);
-            System.out.println("valuesQR" + value);
+            Log.d(LOGTAG,"valuesQR" + value);
         }
         else{
-            System.out.println("valuesQR = null");
+            Log.d(LOGTAG,"valuesQR = error");
         }
         return value;
     }
+
+
 
     private String detectQrcode(Bitmap bitmap) {
         // Bitmap のサイズを取得して、ピクセルデータを取得する
@@ -213,6 +246,9 @@ public class MainService extends KiboRpcService {
             com.google.zxing.Result decodeResult = reader.decode(binaryBitmap);
             // 解析結果を取得する
             String result = decodeResult.getText();
+            if(!(0 < result.length())) {
+                return "error";
+            }
             Log.d("readQR", result);
             return result;
         } catch (Exception e) {
